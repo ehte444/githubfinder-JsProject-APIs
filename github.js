49259c8adb6445ccd90176d3757d43ac3cd0a5e7
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-04-08 20:00:15 +0530

    tweakings

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 class Github {
   constructor() {
-    this.client_id = '05a5d3adaed123c9f765';
-    this.client_secret = 'b24829e8b543aecd97b12484725eb9aea2c1f72a';
+    this.client_id = 'd9308aacf8b204d361fd';
+    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
